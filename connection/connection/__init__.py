a492db055dec3db11c521104381eb07021f3e20a
Andrew Ridyard <andyrids.uncapped698@passmail.net>
2025-05-22 22:16:54 +0100

    docs: update __init__ module docstring.

--- connection/connection/__init__.py
+++ connection/connection/__init__.py
@@ -1,6 +1,17 @@
 # sourcery skip: use-contextlib-suppress
 # pyright: reportMissingImports=false
-"""Connection package __init__ file.
+"""Connection package __init__.
+
+This MicroPython package `__init__` is used to import optional
+extensions for the `connection` package:
+
+- `connection-wlan` (WIP)
+- `connection-mqtt` (TODO)
+- `connection-server` (TODO)
+
+This package uses static typing, which is enabled by the package dependencies.
+On installation, the cross-compiled `typing.mpy` & `typing_extensions.mpy`
+files are downloaded to the device `lib` folder.
 
 Author: Andrew Ridyard.
 

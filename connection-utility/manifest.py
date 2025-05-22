5be0fd0b127e97e25f860f75a2eae38c39d73650
Andrew Ridyard <andyrids.uncapped698@passmail.net>
2025-05-22 22:01:42 +0100

    chore: remove files.

--- connection-utility/manifest.py
+++
@@ -1,7 +0,0 @@
-# metadata(description=None, version=None, license=None, author=None)
-# define metadata for this manifest file. This is useful for micropython-lib packages.
-metadata(description="", version="0.0.1")
-
-# `package(package_path, files=None, base_path='.', opt=None)`
-# if the package isn't in the same directory as the manifest file, use `base_path`.
-package("connection")
